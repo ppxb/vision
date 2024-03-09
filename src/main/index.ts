664144c586d906774643e51c31838c608bbb5711
ppxb <ppxb123@gmail.com>
2024-03-09 17:21:36 +0800

    first commit

---
+++ src/main/index.ts
@@ -0,0 +1,97 @@
+import {
+  app,
+  shell,
+  BrowserWindow,
+  BrowserWindowConstructorOptions
+} from 'electron'
+import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+import { join } from 'path'
+import icon from '../../resources/icon.png?asset'
+
+let mainWindow: BrowserWindow | null
+
+const options: BrowserWindowConstructorOptions = {
+  width: 1024,
+  height: 768,
+  show: false,
+  ...(process.platform === 'linux' ? { icon } : {}),
+  webPreferences: {
+    preload: join(__dirname, '../preload/index.js'),
+    sandbox: false,
+    webSecurity: false,
+    contextIsolation: true
+  }
+}
+
+const createWindow = (): BrowserWindow => {
+  const mainWindow = new BrowserWindow(options)
+
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show()
+  })
+
+  mainWindow.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url)
+    return { action: 'deny' }
+  })
+
+  // HMR for renderer base on electron-vite cli.
+  // Load the remote URL for development or the local html file for production.
+  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  } else {
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'), {
+      hash: '/'
+    })
+  }
+
+  return mainWindow
+}
+
+const initApp = () => {
+  const lock = app.requestSingleInstanceLock()
+
+  if (!lock) {
+    app.quit()
+  } else {
+    app.on('second-instance', () => {
+      if (mainWindow) {
+        if (mainWindow.isMinimized()) {
+          mainWindow.restore()
+        }
+        mainWindow.focus()
+      }
+    })
+
+    app.whenReady().then(() => {
+      electronApp.setAppUserModelId('com.vision')
+
+      app.on('browser-window-created', (_, win) => {
+        optimizer.watchWindowShortcuts(win)
+      })
+
+      // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils#optimizer
+      // optimizer.registerFramelessWindowIpc()
+
+      // const win = createWindow()
+
+      // registerIpc(win)
+
+      createWindow()
+
+      app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+          createWindow()
+        }
+      })
+    })
+
+    app.on('window-all-closed', () => {
+      if (process.platform !== 'darwin') {
+        app.quit()
+      }
+    })
+  }
+}
+
+initApp()

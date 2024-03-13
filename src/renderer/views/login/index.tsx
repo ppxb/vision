03e9e335bcf1bf764dbaebc8d556c096e2d9c086
ppxb <ppxb123@gmail.com>
2024-03-13 23:51:58 +0800

    chore: add login animation

--- src/renderer/views/login/index.tsx
+++ src/renderer/views/login/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
-import { Button } from '@nextui-org/react'
+import { motion } from 'framer-motion'
+import { Button, Spinner } from '@nextui-org/react'
 
 import useAppStore from '@renderer/store'
 import Zenitho from '@renderer/components/zenitho'
@@ -13,6 +14,7 @@ import { CheckIcon, ReloadIcon } from '@renderer/components/icon'
 const LoginView = () => {
   const [qrCode, setQRCode] = useState('')
   const [qrStatus, setQRStatus] = useState('')
+  const [hasToken, setHasToken] = useState(false)
 
   const { token } = useAppStore.use
   const updateToken = useAppStore.use.updateToken()
@@ -50,6 +52,7 @@ const LoginView = () => {
         grant_type: 'authorization_code',
         code
       })
+      setHasToken(true)
       updateToken(data)
     } catch (error) {
       return Promise.reject(error)
@@ -63,29 +66,53 @@ const LoginView = () => {
   return (
     <Zenitho>
       <div className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 flex flex-col items-center justify-center gap-6">
-        <div className="font-logo text-background text-6xl">Vision</div>
-        <div className="flex flex-col items-center p-8 rounded-3xl bg-black/10 backdrop-blur-md backdrop-saturate-150">
-          <div className="relative w-[160px] h-[160px]">
-            {qrStatus === 'QRCodeExpired' && (
-              <div className="absolute inset-0 flex flex-col justify-center items-center gap-2 rounded-2xl bg-black/80">
-                <div className="text-sm text-foreground-50">二维码已过期</div>
-                <Button
-                  size="sm"
-                  startContent={<ReloadIcon />}
-                  onClick={requestQRCode}
-                >
-                  重新加载
-                </Button>
-              </div>
-            )}
-            {qrStatus === 'ScanSuccess' && (
-              <div className="absolute inset-0 flex flex-col justify-center items-center gap-2 rounded-2xl bg-black/80">
-                <CheckIcon className="text-green-500" />
-                <div className="text-sm text-foreground-50">等待 App 确认</div>
-              </div>
-            )}
-            <img src={qrCode} className="rounded-2xl" />
-            {/* {qrCode ? (
+        <motion.div
+          animate={
+            hasToken
+              ? { scale: 2, translateY: 180 }
+              : { scale: 1, translateY: 0 }
+          }
+          transition={{
+            duration: 0.5,
+            ease: [0, 0.71, 0.2, 1.01],
+            delay: 0.3
+          }}
+          className="font-logo text-background text-6xl"
+        >
+          Vision
+        </motion.div>
+        <motion.div
+          animate={
+            hasToken
+              ? { opacity: 0, translateY: '100%' }
+              : { opacity: 1, translateY: '0' }
+          }
+          className="flex flex-col items-center gap-4"
+        >
+          <div className="flex items-center w-fit justify-center p-8 rounded-3xl bg-black/10 backdrop-blur-md backdrop-saturate-150">
+            <div className="relative w-[160px] h-[160px]">
+              {qrStatus === 'QRCodeExpired' && (
+                <div className="absolute inset-0 flex flex-col justify-center items-center gap-2 rounded-2xl bg-black/80">
+                  <div className="text-sm text-foreground-50">二维码已过期</div>
+                  <Button
+                    size="sm"
+                    startContent={<ReloadIcon />}
+                    onClick={requestQRCode}
+                  >
+                    重新加载
+                  </Button>
+                </div>
+              )}
+              {qrStatus === 'ScanSuccess' && (
+                <div className="absolute inset-0 flex flex-col justify-center items-center gap-2 rounded-2xl bg-black/80">
+                  <CheckIcon className="text-green-500" />
+                  <div className="text-sm text-foreground-50">
+                    等待 App 确认
+                  </div>
+                </div>
+              )}
+              <img src={qrCode} className="rounded-2xl" />
+              {/* {qrCode ? (
               <img src={qrCode} className="rounded-2xl" />
             ) : (
               <div className="relative w-[160px] h-[160px] rounded-2xl">
@@ -103,12 +130,39 @@ const LoginView = () => {
                 </div>
               </div>
             )} */}
+            </div>
           </div>
-        </div>
-        <div className="text-center text-sm text-foreground-50">
-          <p className="mb-1">请使用阿里云盘 App 扫描二维码登录</p>
-          <p>Vision 将会被授权使用您的个人信息和云盘信息</p>
-        </div>
+          <div className="text-center text-sm font-light text-foreground-50">
+            <p className="mb-1">请使用阿里云盘 App 扫描二维码登录</p>
+            <p>Vision 将会被授权使用您的个人信息和云盘信息</p>
+          </div>
+        </motion.div>
+        <motion.div
+          animate={
+            hasToken
+              ? {
+                  opacity: 1,
+                  translateY: -110,
+                  visibility: 'visible'
+                }
+              : { opacity: 0 }
+          }
+          transition={{
+            duration: 0.8,
+            ease: [0, 0.71, 0.2, 1.01],
+            delay: 0.3
+          }}
+          className="flex invisible gap-3 font-light text-background"
+        >
+          <Spinner
+            classNames={{
+              wrapper: 'w-5 h-5',
+              circle1: 'border-b-background',
+              circle2: 'border-b-background/80'
+            }}
+          />
+          正在加载您的数据以准备 Vision
+        </motion.div>
       </div>
     </Zenitho>
   )
